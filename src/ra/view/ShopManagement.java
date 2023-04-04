fbba00c122bb805eff64e7fbd17c621aca3272d4
Hung <hunghx@rikkeisoft.com>
2023-04-04 10:58:18 +0700

    hoho

--- src/ra/view/ShopManagement.java
+++ src/ra/view/ShopManagement.java
@@ -9,52 +9,62 @@ import ra.bussiness.notify.ShopMessage;
 import java.util.Scanner;
 
 public class ShopManagement {
+    static User currentUser = null;
     private static CatalogImp catImp = new CatalogImp();
     private static UserImp userImp = new UserImp();
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         do {
-            System.out.println("****************CUA HANG ABC***************");
-            System.out.println("1. Dang nhap");
-            System.out.println("2. Dang ky");
-            System.out.println("3. Thoat");
-            System.out.println("****************CUA HANG ABC***************");
-            System.out.print("Su lua chon cua ban: ");
-            int choice = Integer.parseInt(sc.nextLine());
-            switch (choice){
-                case 1:
-                    login(sc);
-                    break;
-                case 2:
-                    register(sc);
-                    break;
-                case 3:
-                    sc.close();
-                    System.exit(0);
-                default:
-                    System.err.println(ShopMessage.NOTIFY_SHOP_CHOICE);
-            }
-        }while(true);
+            displayMenu(sc);
+        } while (true);
     }
-    public static void login(Scanner sc){
+
+    public static void displayMenu(Scanner sc) {
+        System.out.println("****************CUA HANG ABC***************");
+        System.out.println("1. Dang nhap");
+        System.out.println("2. Dang ky");
+        System.out.println("3. Thoat");
+        System.out.println("****************CUA HANG ABC***************");
+        System.out.print("Su lua chon cua ban: ");
+        int choice = Integer.parseInt(sc.nextLine());
+        switch (choice) {
+            case 1:
+                login(sc);
+                break;
+            case 2:
+                register(sc);
+                break;
+            case 3:
+                sc.close();
+                System.exit(0);
+            default:
+                System.err.println(ShopMessage.NOTIFY_SHOP_CHOICE);
+        }
+    }
+
+    public static void login(Scanner sc) {
         do {
             System.out.println("-----------Đăng Nhập-------------");
             System.out.print("Ten dang nhap: ");
             String userName = sc.nextLine();
             System.out.print("Mat khau: ");
             String password = sc.nextLine();
-            User user = userImp.checkLogin(userName,password);
-            if (user!=null){
+            User user = userImp.checkLogin(userName, password);
+            if (user != null) {
+                currentUser = user;
                 //Dang nhap thanh cong
-                if (user.isPermission()){
+                if (user.isPermission()) {
                     //Tai khoan admin
+
                     displayMenuShopManagement(sc);
-                }else{
+                } else {
                     //Tai khoan user
+
                     displayMenuUser(sc);
                 }
                 break;
-            }else{
+            } else {
                 //Dang nhap that bai
                 System.err.println(ShopMessage.NOTIFY_LOGIN_FAIL);
                 try {
@@ -67,27 +77,28 @@ public class ShopManagement {
                 System.out.println("3. Thoat");
                 System.out.print("Su lua chon: ");
                 int choice = Integer.parseInt(sc.nextLine());
-                if (choice==2){
+                if (choice == 2) {
                     register(sc);
                     //Chuyen den menu dang ky
-                }else if(choice==3){
+                } else if (choice == 3) {
                     break;
                 }
             }
-        }while (true);
+        } while (true);
     }
-    public  static void register(Scanner sc){
+
+    public static void register(Scanner sc) {
         System.out.println("---------Đăng Kí----------------");
         User newUser = userImp.inputData(sc);
         boolean check = userImp.create(newUser);
         if (check) {
             System.out.println("Đăng kí thành công");
-        }else {
+        } else {
             System.err.println("Đăng kí thất bại");
         }
     }
 
-    public static void displayMenuShopManagement(Scanner sc){
+    public static void displayMenuShopManagement(Scanner sc) {
         boolean exit = true;
         do {
             System.out.println("**********************QUAN LY CUA HANG************************");
@@ -98,7 +109,7 @@ public class ShopManagement {
             System.out.println("4. Thoat");
             System.out.print("Lua chon cua ban: ");
             int choice = Integer.parseInt(sc.nextLine());
-            switch (choice){
+            switch (choice) {
                 case 1:
                     displayMenuCatalogManagement(sc);
                     break;
@@ -112,16 +123,38 @@ public class ShopManagement {
                 default:
                     System.err.println(ShopMessage.NOTIFY_SHOP_MANAGEMENT_CHOICE);
             }
-        }while(exit);
+        } while (exit);
 
     }
 
-    public static void displayMenuUser(Scanner sc){
-        System.out.println("Chào mừng bạn đến với cửa hàng của chúng tôi");
+    public static void displayMenuUser(Scanner sc) {
+        System.out.println("Chào mừng " + currentUser.getUserName() + " đến với cửa hàng của chúng tôi");
+        System.out.println("Nhập phím baats kì để tiếp tục");
         sc.nextLine();
+        while (true) {
+
+
+            System.out.println("1.xem cửa hang");
+            System.out.println("2.đăng xuất");
+            System.out.println("lựa chọn của bạn");
+            int n = Integer.parseInt(sc.nextLine());
+            switch (n) {
+                case 1:
+                    System.out.println("list product");
+                    sc.nextLine();
+                    break;
+                case 2:
+                    System.out.println("Good bye " + currentUser.getUserName() + ", see u again");
+                    sc.nextLine();
+                    currentUser = null;
+                    displayMenu(sc);
+                    break;
+            }
+        }
+
     }
 
-    public static void displayMenuCatalogManagement(Scanner sc){
+    public static void displayMenuCatalogManagement(Scanner sc) {
         boolean exit = true;
         do {
             System.out.println("**************QUAN LY DANH MUC**********************");
@@ -133,16 +166,16 @@ public class ShopManagement {
             System.out.println("6. Thoat");
             System.out.print("Lua chon cua ban: ");
             int choice = Integer.parseInt(sc.nextLine());
-            switch (choice){
+            switch (choice) {
                 case 1:
                     catImp.displayData();
                     break;
                 case 2:
                     Catalog catNew = catImp.inputData(sc);
                     boolean result = catImp.create(catNew);
-                    if (result){
+                    if (result) {
                         System.out.println(ShopMessage.NOTIFY_CATALOG_CREATE_SUCCESS);
-                    }else{
+                    } else {
                         System.err.println(ShopMessage.NOTIFY_CATALOG_CREATE_FAIL);
                     }
                     break;
@@ -159,6 +192,6 @@ public class ShopManagement {
                 default:
                     System.err.println(ShopMessage.NOTIFY_CATALOG_MANAGEMENT_CHOICE);
             }
-        }while(exit);
+        } while (exit);
     }
 }
